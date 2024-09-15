d295a8b9fe5cf932be9f96fe1228458dfa064a56
Nilotpal Deka <iamnilotpaldeka@gmail.com>
2024-09-15 18:15:43 +0530

    - Added config package and use godotenv package to load envs. - Construct logger in main and use run function for startup and shutdown services.

--- apps/backend/apps/api/main.go
+++ apps/backend/apps/api/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"os"
+	"runtime"
+
+	"github.com/iamNilotpal/openpulse/foundation/logger"
+	"github.com/joho/godotenv"
+	"go.uber.org/zap"
+)
 
 func main() {
-	fmt.Println("Openpulse main API server.")
+	// Load envs.
+	godotenv.Load()
+
+	log := logger.New("Openpulse Backend")
+	defer log.Sync()
+
+	if err := run(log); err != nil {
+		log.Errorf("Startup", "error", err)
+		log.Sync()
+		os.Exit(1)
+	}
+}
+
+func run(log *zap.SugaredLogger) error {
+	// GOMAXPROCS
+	log.Infow("Startup", "GOMAXPROCS", runtime.GOMAXPROCS(0))
+
+	return nil
 }
