482578c3a60b93a6fa7fd541f67f8373be720a64
Nilotpal Deka <iamnilotpaldeka@gmail.com>
2024-09-25 16:24:30 +0530

    - Update authentication functionality. - Update authorization functionality.

--- apps/backend/business/web/middlewares/authenticate.go
+++ apps/backend/business/web/middlewares/authenticate.go
@@ -2,13 +2,18 @@ package middlewares
 
 import (
 	"net/http"
+	"strconv"
 
+	"github.com/iamNilotpal/openpulse/business/repositories/permissions"
+	"github.com/iamNilotpal/openpulse/business/repositories/roles"
 	"github.com/iamNilotpal/openpulse/business/web/auth"
 	"github.com/iamNilotpal/openpulse/business/web/errors"
 	"github.com/iamNilotpal/openpulse/foundation/web"
 )
 
-func Authenticate(authenticator *auth.Auth) func(http.Handler) http.Handler {
+func Authenticate(
+	authenticator *auth.Auth, roles roles.Repository, permissions permissions.Repository,
+) func(http.Handler) http.Handler {
 	a := func(handler http.Handler) http.Handler {
 		m := func(w http.ResponseWriter, r *http.Request) {
 			token := r.Header.Get("Authorization")
@@ -18,21 +23,74 @@ func Authenticate(authenticator *auth.Auth) func(http.Handler) http.Handler {
 				if auth.IsAuthError(err) {
 					authErr := auth.ExtractAuthError(err)
 
-					web.Error(w, http.StatusUnauthorized, web.APIError{
+					web.Error(w, authErr.Status, web.APIError{
 						Message:   authErr.Error(),
-						ErrorCode: errors.ToString(authErr.Code),
+						ErrorCode: errors.CodeToString(authErr.Code),
 					})
 					return
 				}
 
-				web.Error(w, http.StatusInternalServerError, web.APIError{
-					ErrorCode: errors.ToString(errors.InternalErrorCode),
-					Message:   http.StatusText(http.StatusInternalServerError),
-				})
+				web.Error(
+					w,
+					http.StatusInternalServerError,
+					web.APIError{
+						ErrorCode: errors.CodeToString(errors.InternalServerError),
+						Message:   http.StatusText(http.StatusInternalServerError),
+					})
 				return
 			}
 
-			auth.SetClaims(r.Context(), claims)
+			role, err := roles.QueryById(r.Context(), claims.RoleId)
+			if err != nil {
+				web.Error(
+					w,
+					http.StatusUnauthorized,
+					web.APIError{
+						ErrorCode: errors.CodeToString(errors.Unauthorized),
+						Message:   http.StatusText(http.StatusUnauthorized),
+					},
+				)
+				return
+			}
+
+			userId, err := strconv.Atoi(claims.Subject)
+			if err != nil {
+				web.Error(
+					w,
+					http.StatusUnauthorized,
+					web.APIError{
+						ErrorCode: errors.CodeToString(errors.Unauthorized),
+						Message:   http.StatusText(http.StatusUnauthorized),
+					},
+				)
+				return
+			}
+
+			permissions, err := permissions.QueryByUserId(r.Context(), userId)
+			if err != nil {
+				web.Error(
+					w,
+					http.StatusUnauthorized,
+					web.APIError{
+						ErrorCode: errors.CodeToString(errors.Unauthorized),
+						Message:   http.StatusText(http.StatusUnauthorized),
+					},
+				)
+				return
+			}
+
+			authPermissions := make([]auth.UserPermissions, len(permissions))
+			for i, p := range permissions {
+				authPermissions[i] = auth.ToUserPermissions(
+					auth.ToUserRole(role),
+					auth.ToUserPermission(p),
+				)
+			}
+
+			r = r.WithContext(auth.SetClaims(r.Context(), claims))
+			r = r.WithContext(auth.SetUserRole(r.Context(), auth.ToUserRole(role)))
+			r = r.WithContext(auth.SetUserPermissions(r.Context(), authPermissions))
+
 			handler.ServeHTTP(w, r)
 		}
 
--- apps/backend/business/web/middlewares/authorize.go
+++ apps/backend/business/web/middlewares/authorize.go
@@ -2,12 +2,58 @@ package middlewares
 
 import (
 	"net/http"
+	"strings"
+
+	"github.com/iamNilotpal/openpulse/business/web/auth"
+	"github.com/iamNilotpal/openpulse/business/web/errors"
+	"github.com/iamNilotpal/openpulse/foundation/web"
 )
 
-func Authorize(handler http.Handler) http.Handler {
-	m := func(w http.ResponseWriter, r *http.Request) {
-		handler.ServeHTTP(w, r)
+func Authorize(
+	requiredPermissions ...[]auth.Permissions,
+) func(http.Handler) http.Handler {
+	a := func(handler http.Handler) http.Handler {
+		m := func(w http.ResponseWriter, r *http.Request) {
+			userPermissions := auth.GetUserPermissions(r.Context())
+
+			if !checkPermissions(requiredPermissions, userPermissions) {
+				web.Error(w, http.StatusForbidden, web.APIError{
+					Message:   http.StatusText(http.StatusForbidden),
+					ErrorCode: errors.CodeToString(errors.Forbidden),
+				})
+				return
+			}
+
+			handler.ServeHTTP(w, r)
+		}
+
+		return http.HandlerFunc(m)
 	}
 
-	return http.HandlerFunc(m)
+	return a
+}
+
+func checkPermissions(
+	requiredPermissions [][]auth.Permissions, userPermissions []auth.UserPermissions,
+) bool {
+	if len(requiredPermissions) == 0 {
+		return true
+	}
+
+	for _, rr := range requiredPermissions {
+		for i := 0; i < len(requiredPermissions); i++ {
+			rPermission := rr[i]
+			uPermission := userPermissions[i]
+
+			if uPermission.Permission.Enabled &&
+				uPermission.Role.Id == rPermission.Role.Id &&
+				uPermission.Permission.Id == rPermission.Permission.Id &&
+				strings.EqualFold(uPermission.Permission.Action, rPermission.Permission.Action) &&
+				strings.EqualFold(uPermission.Permission.Resource, rPermission.Permission.Resource) {
+				return true
+			}
+		}
+	}
+
+	return false
 }
