58cfaae4a508db6ffde549a19c4a70c8de1ae654
Nilotpal Deka <iamnilotpaldeka@gmail.com>
2024-10-10 13:35:37 +0530

    Update api response structure to include messge.

--- apps/backend/foundation/web/request.go
+++ apps/backend/foundation/web/request.go
@@ -18,12 +18,12 @@ func GetParam(r *http.Request, key string) string {
 }
 
 // Decode reads the body of an HTTP request looking for a JSON document. The
-// body is decoded into the provided value.
-// If the provided value is a struct then it is checked for validation tags.
-// If the value implements a validate function, it is executed.
+// body is decoded into the provided value. If the provided value is a struct then it is checked for
+// validation tags. If the value implements a validate function, it is executed.
 func Decode(r *http.Request, val any) error {
 	decoder := json.NewDecoder(r.Body)
 	decoder.DisallowUnknownFields()
+
 	if err := decoder.Decode(val); err != nil {
 		return err
 	}
